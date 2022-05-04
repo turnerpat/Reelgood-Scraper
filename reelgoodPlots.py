ef6d363d88413c7e79d73280b89445ef7a2c8deb
Patrick Turner <89099652+turnerpat@users.noreply.github.com>
2022-05-03 20:29:34 -0400

    Add files via upload

--- reelgoodPlots.py
+++ reelgoodPlots.py
@@ -23,18 +23,20 @@ def plot(df):
     # title = 'TV Show IMDb and Reelgood Scores over time'
     # ax = sea.lineplot(data=df_tv, x='year', y='reelgood', ci=None)
     # ax = sea.lineplot(data=df_tv, x='year', y='imdb', ci=None)
+    # ax.legend(['Reelgood', 'IMDb'])
     # ax.set(xlabel='Year', ylabel='Scores', title=title)
 
     # Line Plot of Movie show Reelgood and IMDb scores over time
-    df_mov['year'] = pd.to_datetime(df_mov['year'], errors='coerce')
-    df_mov = df_mov.dropna(subset=['year'])
-    df_mov = df_mov[df_mov['year'].dt.year <= 2022]
-    df_mov = df_mov.sort_values(by='year')
-    df_mov['reelgood'] = df_mov['reelgood'].div(10)
-    title = 'Movie IMDb and Reelgood Scores over time'
-    ax = sea.lineplot(data=df_mov, x='year', y='reelgood', ci=None)
-    ax = sea.lineplot(data=df_mov, x='year', y='imdb', ci=None)
-    ax.set(xlabel='Year', ylabel='Scores', title=title)
+    # df_mov['year'] = pd.to_datetime(df_mov['year'], errors='coerce')
+    # df_mov = df_mov.dropna(subset=['year'])
+    # df_mov = df_mov[df_mov['year'].dt.year <= 2022]
+    # df_mov = df_mov.sort_values(by='year')
+    # df_mov['reelgood'] = df_mov['reelgood'].div(10)
+    # title = 'Movie IMDb and Reelgood Scores over time'
+    # ax = sea.lineplot(data=df_mov, x='year', y='reelgood', ci=None)
+    # ax = sea.lineplot(data=df_mov, x='year', y='imdb', ci=None)
+    # ax.legend(['Reelgood', 'IMDb'])
+    # ax.set(xlabel='Year', ylabel='Scores', title=title)
 
     # Boxplot of IMDb scores for all 3276 Movies and all 5752 TV Shows
     # title = 'IMDb scores of all Movies and TV shows'
@@ -47,6 +49,13 @@ def plot(df):
     # pal = {'TV': "#0ee694", 'Movie': "#e50914"}
     # ax = sea.boxplot(data=df, x="mediatype", y="reelgood", palette=pal)
     # ax.set(xlabel='Media Type', title=title)
+
+    # Joint KDE Plot of Scores for all Movies and TV shows
+    title = 'Joint KDE Plot of Scores for all Movies and TV shows'
+    df['reelgood'] = df['reelgood'].div(10)
+    pal = {'TV': "#0ee694", 'Movie': "#e50914"}
+    ax = sea.jointplot(data=df, x="imdb", y="reelgood", hue="mediatype", kind='kde', palette=pal)
+    ax.fig.suptitle(title)
 
     return ax
 
